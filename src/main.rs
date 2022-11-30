543fe64ba24396dfce67243203107b0e638dec4c
Simone Rosani <s.rosani@anoki.it>
2022-11-30 14:51:46 +0100

    Added if statement example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 2;
+    println!("Is {} less then 5? {}", number, is_less_then_three(number))
+}
+
+fn is_less_then_three(number: i32) -> bool {
+    if number < 5 {
+        true
+    } else {
+        false
+    }
 }
