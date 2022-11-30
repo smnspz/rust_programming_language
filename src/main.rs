abf2a4d085f7cfc17ca75a13d4598e816a319665
Simone Rosani <s.rosani@anoki.it>
2022-11-30 14:57:47 +0100

    Added example of using if inside let statement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let number = 2;
-    println!("Is {} less then 5? {}", number, is_less_then_three(number))
+    println!("Is {} less then 5? {}", number, is_less_then_three(number));
+    println!("2.5 + 2.5 equals 5! {}", expressions_assignment(true));
 }
 
 fn is_less_then_three(number: i32) -> bool {
@@ -9,4 +10,10 @@ fn is_less_then_three(number: i32) -> bool {
     } else {
         false
     }
+}
+
+fn expressions_assignment(condition: bool) -> i32 {
+    let number = if condition { 5 } else { 6 };
+
+    return number;
 }
