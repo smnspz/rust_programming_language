f685dcbe803639179f8d0aea6af5f4af721a0c20
Simone Rosani <s.rosani@anoki.it>
2022-11-30 14:46:34 +0100

    Added basic examples on functions return a arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
+    another_function(five());
+    expressions()
+}
+fn another_function(x: i32) {
+    println!("The value of x is: {}", x);
+}
+
+fn expressions() {
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5 // returns 5 without the need to use the return keyword
 }
