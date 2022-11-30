23f374f3e5bbcc5694e93a6290ad9c2f5e085265
Simone Rosani <s.rosani@anoki.it>
2022-11-30 15:25:55 +0100

    Added scope example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{} World!", scope());
 }
+
+// variable s inside function scope() is not valid here
+
+fn scope() -> &'static str {
+    let s = "Hello"; // s is now valid from this point forward
+                     // do stuff with s
+    return s;
+}
+
+// scope()'s scope is now over, and s is no longer valid
