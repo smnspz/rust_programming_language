3858a5d2712c0eb7142d82dd594499b46214d8ce
Simone Rosani <s.rosani@anoki.it>
2022-12-02 14:15:59 +0100

    Added enums examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,94 @@
 fn main() {
-    println!("Hello, world!");
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let some_number = Some(5);
+    plus_one(some_number);
+
+    let absent_number: Option<i32> = None;
+    plus_one(absent_number);
+
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+}
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// Adds methods to enum
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // etc...
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
+
+// matching with Option<T>
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn the_placeholder() {
+    let some_u8_value = 0u8;
+    // the _ pattern will match any value
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
+
+fn if_let() {
+    let some_u8_value = Some(0u8);
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    // let mut count = 0;
+    // if let Coin::Quarter(State) = coin {
+    //     println!("State quarter from {:?}!", state);
+    // } else {
+    //     count += 1;
+    // }
 }
